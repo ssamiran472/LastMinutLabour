423237df50ea8ece8cbf14c349d8ed122d8e6c22
ssamiran472 <63182995+ssamiran472@users.noreply.github.com>
2020-08-24 16:54:03 +0530

    Update settings.py
    remove my gamil address

--- website/settings.py
+++ website/settings.py
@@ -172,8 +172,8 @@ if not DEBUG:
     EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_PORT = 587
-    EMAIL_HOST_USER = 'samiransarkar513@gmail.com'
-    EMAIL_HOST_PASSWORD = 'Samiran@12'
+    EMAIL_HOST_USER = '##############'
+    EMAIL_HOST_PASSWORD = '################'
     EMAIL_USE_TLS = True
     EMAIL_USE_SSL = False
 
